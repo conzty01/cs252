4fc7f60ae1bee898d71596fa5f19eb58d01c7bdf
conzty01 <conzty01@luther.edu>
2016-11-26 21:06:12 -0600

    Add files via upload

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,42 +1,55 @@
 //Could also be written as public class Fraction extends Object
 
 // Fractions are always stored in reduced form
-
 public class Fraction {
 
     private int bottom;
     private int top;
+    private boolean isDec;
 
     //Constructors for fraction class --> similar to def __init__ in Python
-    public Fraction(int numer, int denom) {
-        
-        int Fractiongcd = gcd(numer, denom);
+    public Fraction(int numer, int denom, boolean prtDec) {
+        int Fractiongcd = gcd(Math.abs(numer), Math.abs(denom));
         top = numer / Fractiongcd;
         bottom = denom / Fractiongcd;
+        isDec = prtDec;
         /* Could also be written as:
                     this.top = numer;
                     this.bottom = denom; */
     }
-    
-    public Fraction(int numer) {
-        this(numer, 1);
+
+    public Fraction(int numer, boolean prtDec) {
+        this(numer, 1, prtDec);
     }
-    
+
     public int getNumer() {
         return this.top;
     }
-    
+
     public int getDenom() {
         return this.bottom;
     }
-    
+
+    public boolean getIsDec() {
+        return this.isDec;
+    }
+
     @Override
     public String toString() {
-        return top + "/" + bottom;
+        if (bottom == 1) {
+            return String.valueOf(top);
+        } else if (top < 0) {
+            return "~" + (0 - top) + "/" + bottom;
+        } else if (isDec) {
+            double res = top / bottom;
+            return Double.toString(res);
+        } else {
+            return top + "/" + bottom;
+        }
     }
-    
+
     public Double getDec() {
-        return (double)top / (double)bottom;
+        return (double) top / (double) bottom;
     }
 
     public Fraction add(Fraction other) {
@@ -46,48 +59,48 @@ public class Fraction {
         sumTop = this.top * other.bottom + other.top * this.bottom;
         sumBottom = other.bottom * this.bottom;
 
-        Fraction answer = new Fraction(sumTop, sumBottom);
+        Fraction answer = new Fraction(sumTop, sumBottom, this.getIsDec() || other.getIsDec());
 
         return answer;
 
     }
-    
+
     public Fraction subtract(Fraction other) {
         int difTop;
         int difBottom;
-        
-        difTop = other.top *this.bottom - this.top * other.bottom;
+
+        difTop = other.top * this.bottom - this.top * other.bottom;
         difBottom = other.bottom * this.bottom;
-        
-        Fraction answer = new Fraction(difTop, difBottom);
-        
+
+        Fraction answer = new Fraction(difTop, difBottom, this.getIsDec() || other.getIsDec());
+
         return answer;
     }
-    
+
     public Fraction multiply(Fraction other) {
         int productTop;
         int productBottom;
-        
+
         productTop = this.top * other.top;
         productBottom = this.bottom * other.bottom;
-        
-        Fraction answer = new Fraction(productTop, productBottom);
-        
+
+        Fraction answer = new Fraction(productTop, productBottom, this.getIsDec() || other.getIsDec());
+
         return answer;
-        
+
     }
-    
+
     public Fraction divide(Fraction other) {
         int quotientTop;
         int quotientBottom;
-        
+
         quotientTop = this.top * other.bottom;
         quotientBottom = this.bottom * other.top;
-        
-        Fraction answer = new Fraction(quotientTop, quotientBottom);
-        
+
+        Fraction answer = new Fraction(quotientTop, quotientBottom, this.getIsDec() || other.getIsDec());
+
         return answer;
-        
+
     }
 
     public static int gcd(int x, int y) {
--- src/MyStack.java
+++ src/MyStack.java
@@ -8,34 +8,38 @@
  *
  * @author conzty01
  */
-
 import java.util.ArrayList;
 
 public class MyStack<T> {
+
     private ArrayList<T> lst;
+
     public MyStack() {
         lst = new ArrayList();
     }
+
     public void push(T o) {
         lst.add(o);
     }
+
     public T pop() {
-        return lst.remove(lst.size()-1);
+        return lst.remove(lst.size() - 1);
     }
+
     public boolean isEmpty() {
         return (lst.size() == 0);
     }
+
     public int stackLen() {
         return lst.size();
     }
-    @Override
+
     public String toString() {
         String str = "";
-        
-        for(int i = 0; i >= lst.size(); i++) {
+
+        for (int i = 0; i < lst.size(); i++) {
             str += lst.get(i).toString() + "\n";
         }
         return str;
     }
-
 }
